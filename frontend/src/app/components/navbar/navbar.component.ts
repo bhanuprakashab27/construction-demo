4eb93ad04a9d9bf5170bbca7dd16f789088e7b2d
Bhanuprakasha B <bhanuprakashab99@gmail.com>
2026-03-15 20:06:01 +0530

    contact page integration added

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -4,10 +4,20 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink,CommonModule],
+  imports: [RouterLink,CommonModule, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  closeMenu(){
+
+const menu = document.getElementById('navMenu');
+
+if(menu?.classList.contains('show')){
+menu.classList.remove('show');
+}
+
+}
 
 }
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Service } from '../../entity/service';
 import { DataService } from '../../services/data.service';
 import { CommonModule, NgFor } from '@angular/common';
 import { Project } from '../../entity/project';
 import { WhyChoose } from '../../entity/whychoose';
+import Swiper from 'swiper';
+import { Pagination, Autoplay } from 'swiper/modules';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +13,9 @@ import { WhyChoose } from '../../entity/whychoose';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
+    @ViewChild('scrollContainer') scrollContainer!: ElementRef;
   services: Service[] = [];
   projects: Project[] = [];
   whyChoose: WhyChoose[] = [];
@@ -20,6 +23,48 @@ export class HomeComponent {
 
 
   constructor(private dataService: DataService) {}
+
+intervalId:any;
+
+ngAfterViewInit(){
+
+new Swiper('.testimonialSwiper',{
+
+modules:[Pagination,Autoplay],
+
+loop:true,
+
+spaceBetween:30,
+
+autoplay:{
+delay:3000,
+disableOnInteraction:false
+},
+
+pagination:{
+el:'.swiper-pagination',
+clickable:true
+},
+
+breakpoints:{
+
+0:{
+slidesPerView:1
+},
+
+768:{
+slidesPerView:2
+},
+
+1024:{
+slidesPerView:3
+}
+
+}
+
+});
+
+}
 
   ngOnInit() {
     this.dataService.getServices().subscribe(data => {
