3820e75d0f8305de55dd736b7016377c95acbc52
Bhanuprakasha B <bhanuprakashab99@gmail.com>
2026-04-02 15:40:48 +0530

    env added

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -2,13 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Service } from '../entity/service';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-   private apiUrl = "http://localhost:8080/api";
+   private apiUrl = environment.apiUrl + '/api';
+
   constructor(private http: HttpClient) {}
 
   getServices(): Observable<Service[]> {
